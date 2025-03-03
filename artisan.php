5e27481b9309fe646331beaa7afd2d2650f39849
toranit2541 <toranit.won.41@gmail.com>
2025-03-03 15:17:06 +0700

    Update artisan.php

--- artisan.php
+++ artisan.php
@@ -1,8 +1,5 @@
 <?php
   echo "<pre>";
   echo shell_exec("php artisan migrate 2>&1");
-  echo shell_exec("php artisan config:cache 2>&1");
-  echo shell_exec("php artisan route:cache 2>&1");
-  echo shell_exec("php artisan cache:clear 2>&1");
   echo "</pre>";
 ?>
