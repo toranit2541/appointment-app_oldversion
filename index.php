ed9ee7577971d0f1df539937ee7cbc1f4ed9a4c3
toranit2541 <toranit.won.41@gmail.com>
2025-03-10 10:33:11 +0700

    'sendindex'

--- index.php
+++ index.php
@@ -46,9 +46,9 @@ require __DIR__.'/../bootstrap/autoload.php';
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
 
