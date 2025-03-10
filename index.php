3f85132b5e40ebc940281503d23799f83ed0cb55
toranit2541 <toranit.won.41@gmail.com>
2025-03-10 09:47:33 +0700

    'sendto'

--- index.php
+++ index.php
@@ -46,6 +46,8 @@ require __DIR__.'/../bootstrap/autoload.php';
 |
 */
 
+require __DIR__.'/../vendor/autoload.php';
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Kernel::class);
